78ba74ba7fe4d4a43238c5c938a5d2f54f0a985c
Poulain Thomas <tpoulain@asi-testing>
2019-12-02 17:44:41 +0100

    mise à jour

---
+++ exa2961/src/main/java/org/prodageo/exa2961/App.java
@@ -0,0 +1,13 @@
+package org.prodageo.exa2961;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
